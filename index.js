3e8f30dafbb32e868159259111cbc06312b919c3
CodeByFatima <fatimamadeit@gmail.com>
2025-11-17 20:34:27 +0500

    added bit of transitions

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Js check")
+const sidebar = document.querySelector(".sidebar")
+const sidebarToggler = document.querySelector(".sidebar-toggler")
+
+//Toggle sidebar's collapse state
+sidebarToggler.addEventListener("click", () => {
+sidebar.classList.toggle("collapsed")
+})
